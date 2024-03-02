b235f09f879ef5c7b2ec2d9c7dcf6149902aad5e
ficky230596 <129863289+ficky230596@users.noreply.github.com>
2024-03-02 22:23:05 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,6 @@
 <?php 
 // Membuat koneksi ke database MySQL
 $koneksi = mysqli_connect("localhost", "root", "", "tugasweb");
- 
 // Memeriksa koneksi ke database
 if (mysqli_connect_errno()) {
     // Jika koneksi gagal, tampilkan pesan kesalahan
